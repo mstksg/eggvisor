9a0141265ca98017737c9255d562e8e23d6ee116
Justin Le <justin@jle.im>
2017-05-31 21:03:25 -0700

    safe indexing with types

---
+++ src/Data/Type/Combinator/Util.hs
@@ -0,0 +1,135 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE ConstraintKinds     #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE EmptyCase           #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE PolyKinds           #-}
+{-# LANGUAGE RankNTypes          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE TypeInType          #-}
+{-# LANGUAGE TypeOperators       #-}
+
+module Data.Type.Combinator.Util (
+    HasLen(..)
+  , withProd
+  , unzipP
+  , zipP
+  , allWit
+  , allWit'
+  , tagSum
+  , sumIso
+  , sumSome
+  , someSum
+  , ixProd
+  , _Flip
+  ) where
+
+import           Control.Lens hiding ((:<), Index)
+import           Data.Kind
+import           Data.Type.Combinator
+import           Data.Type.Conjunction
+import           Data.Type.Index
+import           Data.Type.Length
+import           Data.Type.Nat
+import           Data.Type.Product
+import           Data.Type.Sum
+import           Data.Type.Vector
+import           Type.Class.Higher
+import           Type.Class.Witness
+import           Type.Family.List
+import           Type.Family.Nat
+
+data HasLen :: N -> [k] -> Type where
+    HLZ :: HasLen 'Z '[]
+    HLS :: HasLen n as -> HasLen ('S n) (a ': as)
+
+withProd
+    :: forall n f g a. ()
+    => (f a -> Some g)
+    -> VecT n f a
+    -> Some (HasLen n :&: Prod g)
+withProd f = go
+  where
+    go :: VecT m f a -> Some (HasLen m :&: Prod g)
+    go = \case
+      ØV      -> Some (HLZ :&: Ø)
+      x :* xs -> some (f x)   $ \y ->
+                 some (go xs) $ \(hl :&: ys) ->
+        Some (HLS hl :&: (y :< ys))
+
+unzipP
+    :: Prod (f :&: g) as
+    -> (Prod f :&: Prod g) as
+unzipP = \case
+    Ø               -> Ø :&: Ø
+    (x :&: y) :< zs -> case unzipP zs of
+      xs :&: ys -> (x :< xs) :&: (y :< ys)
+
+zipP
+    :: (Prod f :&: Prod g) as
+    -> Prod (f :&: g) as
+zipP = \case
+    Ø         :&: Ø         -> Ø
+    (x :< xs) :&: (y :< ys) -> (x :&: y) :< zipP (xs :&: ys)
+
+allWit'
+    :: Prod (Wit :.: c) as
+    -> Wit (ListC (c <$> as))
+allWit' = \case
+    Ø           -> Wit
+    Comp Wit :< wits -> case allWit' wits of
+      Wit -> Wit
+
+allWit
+    :: forall f c as. ()
+    => (forall a. f a -> Wit (c a))
+    -> Prod f as
+    -> Wit (ListC (c <$> as))
+allWit f = allWit' . map1 (Comp . f)
+
+tagSum
+    :: Prod f as
+    -> Sum g as
+    -> Sum (f :&: g) as
+tagSum = \case
+    Ø       -> \case
+    x :< xs -> \case
+      InL y  -> InL (x :&: y)
+      InR ys -> InR (tagSum xs ys)
+
+sumIso :: Iso' (Sum f as) (Some (Index as :&: f))
+sumIso = iso sumSome someSum
+
+sumSome :: Sum f as -> Some (Index as :&: f)
+sumSome = \case
+    InL x  -> Some (IZ :&: x)
+    InR xs -> some (sumSome xs) $ \(ix :&: y) ->
+                Some (IS ix :&: y)
+
+someSum :: forall f as. Some (Index as :&: f) -> Sum f as
+someSum = withSome (uncurryFan go)
+  where
+    go :: forall bs a. Index bs a -> f a -> Sum f bs
+    go = \case
+      IZ   -> InL
+      IS i -> \x -> InR (go i x)
+
+ixProd :: Index as a -> Lens' (Prod f as) (f a)
+ixProd = \case
+    IZ -> \f -> \case
+      x :< xs -> (:< xs) <$> f x
+    IS i -> \f -> \case
+      x :< xs -> (x :<) <$> ixProd i f xs
+
+_Flip :: Lens (Flip f a b) (Flip f c d) (f b a) (f d c)
+_Flip f (Flip x) = Flip <$> f x
+
+-- sumProd
+--     :: (Sum f :&: Prod g) as -> Sum (f :&: g) as
+
+-- sumProd :: Sum f as -> Lens' (Prod g as) (Some (f :&: g))
+-- sumProd = \case
+--     InL x -> \f -> \case
+--       y :< ys -> f (Some (x :&: y)) <&> \case Some (x' :&: y') -> y' :< ys
--- src/Egg/Upgrade.hs
+++ src/Egg/Upgrade.hs
@@ -1,10 +1,18 @@
-{-# LANGUAGE DeriveGeneric       #-}
-{-# LANGUAGE GADTs               #-}
-{-# LANGUAGE KindSignatures      #-}
-{-# LANGUAGE LambdaCase          #-}
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE RecordWildCards     #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE DeriveGeneric        #-}
+{-# LANGUAGE FlexibleContexts     #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE GADTs                #-}
+{-# LANGUAGE KindSignatures       #-}
+{-# LANGUAGE LambdaCase           #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE RankNTypes           #-}
+{-# LANGUAGE RecordWildCards      #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
+{-# LANGUAGE TypeApplications     #-}
+{-# LANGUAGE TypeOperators        #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE ViewPatterns         #-}
 
 module Egg.Upgrade (
     BonusAmount(..)
@@ -21,27 +29,46 @@ module Egg.Upgrade (
   , researchBonuses
   , totalBonuses
   , foldResearch
-  , purchaseCommon
+  -- , purchaseCommon
   ) where
 
-import           Control.Applicative
+import           Control.Applicative hiding      (some)
+import           Control.Lens hiding             ((.=))
+import           Control.Monad
 import           Control.Monad.Trans.Class
 import           Control.Monad.Trans.Except
 import           Control.Monad.Trans.State
 import           Data.Aeson.Encoding
 import           Data.Aeson.Types
+import           Data.Dependent.Sum
+import           Data.Finite
 import           Data.Foldable
+import           Data.Function
 import           Data.Kind
+import           Data.Singletons
+import           Data.Singletons.Prelude hiding  (Flip)
+import           Data.Singletons.TypeLits
+import           Data.Type.Combinator
+import           Data.Type.Combinator.Singletons
+import           Data.Type.Combinator.Util
+import           Data.Type.Conjunction
+import           Data.Type.Index
+import           Data.Type.Product               as TCP
+import           Data.Type.Sum
+import           Data.Type.Vector
 import           Data.Yaml
 import           Egg.Types
-import           GHC.Generics               (Generic)
+import           GHC.Generics                    (Generic)
 import           GHC.TypeLits
 import           Numeric.Natural
-import qualified Data.HashMap.Lazy          as HM
-import qualified Data.Map                   as M
-import qualified Data.Text                  as T
-import qualified Data.Text.Encoding         as T
-import qualified Data.Vector                as V
+import           Type.Class.Higher
+import           Type.Class.Witness
+import qualified Data.HashMap.Lazy               as HM
+import qualified Data.Map                        as M
+import qualified Data.Text                       as T
+import qualified Data.Text.Encoding              as T
+import qualified Data.Vector                     as V
+import qualified Data.Vector.Sized               as SV
 
 data BonusAmount = BAIncrement Double
                  | BAPercent Double
@@ -87,17 +114,27 @@ data Research a =
              }
   deriving (Show, Eq, Ord)
 
-data ResearchData =
-    ResearchData { rdCommon :: V.Vector (V.Vector (Research Double))
-                 , rdEpic   :: V.Vector (Research Integer)
-                 }
-  deriving Show
+data ResearchData :: [Nat] -> Nat -> Type where
+    ResearchData
+        :: { rdCommon :: Prod (Flip SV.Vector (Research Double)) tiers
+           , rdEpic   :: SV.Vector epic (Research Integer)
+           }
+        -> ResearchData tiers epic
+    deriving (Show, Eq, Ord)
 
-data ResearchStatus =
-    ResearchStatus { rsCommon :: V.Vector (V.Vector Natural)
-                   , rsEpic   :: V.Vector Natural
-                   }
-  deriving Show
+type SomeResearchData = DSum Sing (Uncur ResearchData)
+
+data ResearchStatus :: [Nat] -> Nat -> Type where
+    ResearchStatus
+        :: { rsCommon :: Prod (Flip SV.Vector Natural) tiers
+           , rsEpic   :: SV.Vector epic Natural
+           }
+        -> ResearchStatus tiers epic
+  deriving (Show, Eq, Ord)
+
+data ResearchIx :: [Nat] -> Nat -> Type -> Type where
+    RICommon :: Prod Finite tiers -> ResearchIx tiers epic Double
+    RIEpic   :: Finite epic       -> ResearchIx tiers epic Integer
 
 bonusAmountParseOptions :: Options
 bonusAmountParseOptions = defaultOptions
@@ -162,18 +199,32 @@ instance ToJSON a => ToJSON (Research a) where
                          Right cs -> "costs"  .= cs
         ]
 
-instance FromJSON ResearchData where
-    parseJSON = withObject "ResearchData" $ \v ->
-      ResearchData <$> v .: "research"
-                   <*> v .: "epic"
-instance ToJSON ResearchData where
+instance FromJSON SomeResearchData where
+    parseJSON = withObject "ResearchData" $ \v -> do
+        res   <- v .: "research"
+        epics <- v .: "epic"
+        withV res $ \resV ->
+          some (withProd (f . getI) resV) $ \(_ :&: (unzipP->(resS :&: resP))) ->
+            SV.withSized epics   $ \sizedV ->
+              return (STuple2 (prodSing resS) SNat :=> Uncur (ResearchData resP sizedV))
+      where
+        f   :: V.Vector (Research Double)
+            -> Some (Sing :&: Flip SV.Vector (Research Double))
+        f xs = SV.withSized xs $ \xsV -> Some (SNat :&: Flip xsV)
+
+instance ToJSON SomeResearchData where
+    toJSON = \case
+        s :=> Uncur r -> toJSON r
+    toEncoding = \case
+        s :=> Uncur r -> toEncoding r
+instance ToJSON (ResearchData tiers epic) where
     toJSON ResearchData{..} = object
-        [ "research" .= rdCommon
-        , "epic"     .= rdEpic
+        [ "research" .= TCP.toList (SV.fromSized . getFlip) rdCommon
+        , "epic"     .= SV.fromSized rdEpic
         ]
     toEncoding ResearchData{..} = pairs . mconcat $
-        [ "research" .= rdCommon
-        , "epic"     .= rdEpic
+        [ "research" .= TCP.toList (SV.fromSized . getFlip) rdCommon
+        , "epic"     .= SV.fromSized rdEpic
         ]
 
 
@@ -195,60 +246,128 @@ bonuses = Bonuses . M.filter (not . null) . fmap (filter hasEffect)
 maxLevel :: Research a -> Int
 maxLevel = either fromIntegral V.length . rCosts
 
-emptyStatus :: ResearchData -> ResearchStatus
+emptyStatus :: ResearchData tiers epic -> ResearchStatus tiers epic
 emptyStatus ResearchData{..} =
-    ResearchStatus (fmap (0 <$) rdCommon) (0 <$ rdEpic)
+    ResearchStatus (map1 (mapFlip (0 <$)) rdCommon) (0 <$ rdEpic)
 
 foldResearch
     :: Monoid b
     => (Either (Research Double) (Research Integer) -> Natural -> b)
-    -> ResearchData
-    -> ResearchStatus
+    -> ResearchData tiers epic
+    -> ResearchStatus tiers epic
     -> b
 foldResearch f ResearchData{..} ResearchStatus{..} = mconcat
-    [ (foldZip . foldZip) (f . Left) rdCommon rsCommon
-    , foldZip (f . Right) rdEpic rsEpic
+    [ foldMap1 (\case Flip d :&: Flip s -> fold $ SV.zipWith (f . Left) d s
+               )
+        (zipP (rdCommon :&: rsCommon))
+    , fold $ SV.zipWith (f . Right) rdEpic rsEpic
     ]
-  where
-    foldZip :: Monoid c => (a -> b -> c) -> V.Vector a -> V.Vector b -> c
-    foldZip f x y = fold $ V.zipWith f x y
 
 researchBonuses :: Research a -> Natural -> Bonuses
 researchBonuses _ 0 = Bonuses M.empty
 researchBonuses Research{..} s = case rBaseBonuses of
     Bonuses m -> Bonuses $ map (scaleAmount s) <$> m
 
-totalBonuses :: ResearchData -> ResearchStatus -> Bonuses
+totalBonuses :: ResearchData tiers epic -> ResearchStatus tiers epic -> Bonuses
 totalBonuses = foldResearch (either researchBonuses researchBonuses)
 
-data ResearchError = REBadTier
-                   | REBadSlot
-                   | REMaxedOut
-  deriving (Show, Eq, Ord)
-
 purchaseCommon
-    :: ResearchData
-    -> Int
-    -> Int
-    -> ResearchStatus
-    -> Either ResearchError (Maybe Double, ResearchStatus)
-purchaseCommon ResearchData{..} t r = runStateT $ do
-    tier <- maybe (lift (Left REBadTier)) return $ rdCommon V.!? t
-    res  <- maybe (lift (Left REBadSlot)) return $ tier     V.!? r
-    currCommon <- gets rsCommon
-    currTier <- maybe (lift (Left REBadTier)) return $ currCommon V.!? t
-    currRes  <- maybe (lift (Left REBadSlot)) return $ currTier   V.!? r
-    let newLevel = currRes + 1
-        updated  = currCommon V.// [(t, currTier V.// [(r, fromIntegral newLevel)])]
-    case rCosts res of
-      Left m | newLevel > m -> lift $ Left REMaxedOut
-             | otherwise    -> do
-                 modify $ \rs -> rs { rsCommon = updated }
-                 return Nothing
-      Right v -> case v V.!? fromIntegral newLevel of
-        Nothing -> lift $ Left REMaxedOut
-        Just p  -> do
-          modify $ \rs -> rs { rsCommon = updated }
-          return (Just p)
+    :: forall tiers epic. Every KnownNat tiers
+    => ResearchData tiers epic
+    -> Sum Finite tiers
+    -> ResearchStatus tiers epic
+    -> (Maybe Double, ResearchStatus tiers epic)
+purchaseCommon ResearchData{..} ix rs0 =
+    some (sumSome pickedTier) $ \(tier :&: ((Flip d :&: Flip s) :&: slot)) ->
+      case every @_ @KnownNat tier of
+        Wit -> flip (_rsCommon . ixProd tier . _Flip . ixSV slot) rs0 $ \currLevel ->
+          case rCosts (SV.index d slot) of
+            Left m   -> if currLevel < m
+              then (Just 0 , currLevel + 1)
+              else (Nothing, currLevel)
+            Right cs -> case cs V.!? fromIntegral (currLevel + 1) of
+              Just c  -> (Just c , currLevel + 1)
+              Nothing -> (Nothing, currLevel)
+  where
+    pickedTier
+        :: Sum ((Flip SV.Vector (Research Double) :&: Flip SV.Vector Natural) :&: Finite) tiers
+    pickedTier = tagSum (zipP (rdCommon :&: rsCommon rs0)) ix
+
+purchaseEpic
+    :: forall tiers epic. KnownNat epic
+    => ResearchData tiers epic
+    -> Finite epic
+    -> ResearchStatus tiers epic
+    -> (Maybe Integer, ResearchStatus tiers epic)
+purchaseEpic ResearchData{..} ix rs0 = flip (_rsEpic . ixSV ix) rs0 $ \currLevel ->
+    case rCosts (SV.index rdEpic ix) of
+      Left m   -> if currLevel < m
+        then (Just 0 , currLevel + 1)
+        else (Nothing, currLevel)
+      Right cs -> case cs V.!? fromIntegral (currLevel + 1) of
+        Just c  -> (Just c , currLevel + 1)
+        Nothing -> (Nothing, currLevel)
+
+ixSV :: KnownNat n => Finite n -> Lens' (SV.Vector n a) a
+ixSV ix f v = (\x -> v SV.// [(fromIntegral ix, x)]) <$> f (SV.index v ix)
+
+-- purchaseCommon ResearchData{..} t r = runStateT $ do
+--     tier <- maybe (lift (Left REBadTier)) return $ rdCommon V.!? t
+--     res  <- maybe (lift (Left REBadSlot)) return $ tier     V.!? r
+--     currCommon <- gets rsCommon
+--     currTier <- maybe (lift (Left REBadTier)) return $ currCommon V.!? t
+--     currRes  <- maybe (lift (Left REBadSlot)) return $ currTier   V.!? r
+--     let newLevel = currRes + 1
+--         updated  = currCommon V.// [(t, currTier V.// [(r, fromIntegral newLevel)])]
+--     case rCosts res of
+--       Left m | newLevel > m -> lift $ Left REMaxedOut
+--              | otherwise    -> do
+--                  modify $ \rs -> rs { rsCommon = updated }
+--                  return Nothing
+--       Right v -> case v V.!? fromIntegral newLevel of
+--         Nothing -> lift $ Left REMaxedOut
+--         Just p  -> do
+--           modify $ \rs -> rs { rsCommon = updated }
+--           return (Just p)
 
 
+-- -- data ResearchError = REBadTier
+-- --                    | REBadSlot
+-- --                    | REMaxedOut
+-- --   deriving (Show, Eq, Ord)
+
+-- -- purchaseCommon
+-- --     :: ResearchData
+-- --     -> Int
+-- --     -> Int
+-- --     -> ResearchStatus
+-- --     -> Either ResearchError (Maybe Double, ResearchStatus)
+-- -- purchaseCommon ResearchData{..} t r = runStateT $ do
+-- --     tier <- maybe (lift (Left REBadTier)) return $ rdCommon V.!? t
+-- --     res  <- maybe (lift (Left REBadSlot)) return $ tier     V.!? r
+-- --     currCommon <- gets rsCommon
+-- --     currTier <- maybe (lift (Left REBadTier)) return $ currCommon V.!? t
+-- --     currRes  <- maybe (lift (Left REBadSlot)) return $ currTier   V.!? r
+-- --     let newLevel = currRes + 1
+-- --         updated  = currCommon V.// [(t, currTier V.// [(r, fromIntegral newLevel)])]
+-- --     case rCosts res of
+-- --       Left m | newLevel > m -> lift $ Left REMaxedOut
+-- --              | otherwise    -> do
+-- --                  modify $ \rs -> rs { rsCommon = updated }
+-- --                  return Nothing
+-- --       Right v -> case v V.!? fromIntegral newLevel of
+-- --         Nothing -> lift $ Left REMaxedOut
+-- --         Just p  -> do
+-- --           modify $ \rs -> rs { rsCommon = updated }
+-- --           return (Just p)
+
+_rsCommon :: Lens (ResearchStatus t1 e)
+                  (ResearchStatus t2 e)
+                  (Prod (Flip SV.Vector Natural) t1)
+                  (Prod (Flip SV.Vector Natural) t2)
+_rsCommon f rs = (\v -> rs { rsCommon = v }) <$> f (rsCommon rs)
+
+        -- :: { rdCommon :: Prod (Flip SV.Vector (Research Double)) tiers
+
+_rsEpic :: Lens (ResearchStatus t e1) (ResearchStatus t e2) (SV.Vector e1 Natural) (SV.Vector e2 Natural)
+_rsEpic f rs = (\v -> rs { rsEpic = v }) <$> f (rsEpic rs)
