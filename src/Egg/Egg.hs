a5058af2e27f85f882ee870bf1eba77d67c52f51
Justin Le <justin@jle.im>
2017-06-05 22:05:43 -0700

    Egg

--- src/Egg/Egg.hs
+++ src/Egg/Egg.hs
@@ -1,22 +1,85 @@
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE InstanceSigs          #-}
+{-# LANGUAGE LambdaCase            #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE RecordWildCards       #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
 
 module Egg.Egg (
+    Egg(..)
+  , eggName, eggDescription, eggValue, eggDiscover, eggUnlock, eggMultiplier
+  , EggData(..), _EggData
+  , SomeEggData
   ) where
 
+import           Control.Lens hiding      ((.=))
+import           Data.Aeson
+import           Data.Aeson.Types
+import           Data.Dependent.Sum
+import           Data.Singletons
+import           Data.Singletons.TypeLits
+import           Egg.Types
+import           GHC.Generics             (Generic)
+import           Text.Printf
+import qualified Data.Text                as T
+import qualified Data.Vector.Sized        as SV
 
--- data Egg = Egg { _eggName :: T.Text
---                , _eggDescription :: T.Text
---                , _eggValue       :: Double
---                , _egg
---                }
+data Egg = Egg { _eggName        :: T.Text
+               , _eggDescription :: T.Text
+               , _eggValue       :: Bock
+               , _eggDiscover    :: Bock
+               , _eggUnlock      :: Bock
+               , _eggMultiplier  :: Double
+               }
+  deriving (Show, Eq, Ord, Generic)
 
-    -- value: 0.25
-    -- discover: 0
-    -- unlock: 0
--- data Hab = Hab { _habName         :: T.Text
---                , _habBaseCapacity :: Natural
---                , _habCosts        :: Vec N4 Double
---                }
---   deriving (Show, Eq, Ord, Generic)
+makeLenses ''Egg
 
+data EggData eggs = EggData { _edEggs :: SV.Vector eggs Egg }
+  deriving (Show, Eq, Ord, Generic)
 
+makePrisms ''EggData
+makeWrapped ''EggData
 
+type SomeEggData = DSum Sing EggData
+
+eggParseOptions :: Options
+eggParseOptions = defaultOptions
+    { fieldLabelModifier = camelTo2 '-' . drop 4
+    }
+
+instance FromJSON Egg where
+    parseJSON  = genericParseJSON  eggParseOptions
+instance ToJSON Egg where
+    toJSON     = genericToJSON     eggParseOptions
+    toEncoding = genericToEncoding eggParseOptions
+
+instance FromJSON SomeEggData where
+    parseJSON = withObject "EggData" $ \v -> do
+      es <- v .: "eggs"
+      SV.withSized es $ \esV ->
+        return $ SNat :=> EggData esV
+instance ToJSON SomeEggData where
+    toJSON = \case
+        _ :=> r -> toJSON r
+    toEncoding = \case
+        _ :=> r -> toEncoding r
+instance KnownNat eggs => FromJSON (EggData eggs) where
+    parseJSON = withObject "EggData" $ \v -> do
+      es <- v .: "eggs"
+      case SV.toSized es of
+        Nothing  -> fail $ printf "Bad number of items in list. (Expected %d, got %d)"
+                         (fromSing (SNat @eggs)) (length es)
+        Just esV -> return $ EggData esV
+instance ToJSON (EggData eggs) where
+    toJSON EggData{..} = object
+        [ "eggs" .= SV.fromSized _edEggs
+        ]
+    toEncoding EggData{..} = pairs . mconcat $
+        [ "eggs" .= SV.fromSized _edEggs
+        ]
---
+++ src/Egg/GameData.hs
@@ -0,0 +1,5 @@
+
+module Egg.GameData (
+  ) where
+
+
