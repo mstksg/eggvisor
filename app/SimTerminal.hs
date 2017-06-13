04cfffac0a8ce22522b6f193187e91c9ad273520
Justin Le <justin@jle.im>
2017-06-12 17:17:00 -0700

    uller hab viewing

--- app/SimTerminal.hs
+++ app/SimTerminal.hs
@@ -9,24 +9,55 @@
 
 import           Brick
 import           Brick.BChan
+import           Brick.Widgets.Center
+import           Brick.Widgets.ProgressBar
 import           Control.Concurrent
 import           Control.Lens
 import           Control.Monad
+import           Data.Foldable
+import           Data.Maybe
 import           Data.Proxy
 import           Data.Singletons
 import           Data.Singletons.TypeLits
+import           Data.Text.Lens
+import           Data.Type.Combinator
 import           Data.Type.Equality
 import           Data.Vector.Sized.Util
 import           Data.Yaml
-import           Egg.Action
-import           Egg.Egg
-import           Egg.Farm
-import           Egg.GameData
-import           Egg.Habitat
+import           Egg
 import           GHC.TypeLits.Compare
 import           Graphics.Vty
-import qualified GHC.TypeLits             as TL
+import           Text.Printf
+import qualified GHC.TypeLits              as TL
 
+renderApp
+    :: (KnownNat eggs, SingI tiers, KnownNat epic, KnownNat habs, KnownNat vehicles, 1 TL.<= eggs, 1 TL.<= habs, 1 TL.<= vehicles)
+    => GameData eggs '(tiers, epic) habs vehicles
+    -> FarmStatus eggs '(tiers, epic) habs vehicles
+    -> Widget n
+renderApp gd fs = vBox
+    [ str "Egg Simulator"
+    , txt $ gd ^. gdEggData . _EggData . ixSV (fs ^. fsEgg) . eggName
+    , str . show $ fs ^. fsBocks
+    , habs
+    ]
+  where
+    bs = farmBonuses gd fs
+    habs = vBox $
+        progShow "All Habs" (sum habCaps - sum (Comp avails)) (sum habCaps)
+        : fold ((\h c a -> return $ case h of
+                    Nothing -> hCenter . str $ "(No hab in slot)"
+                    Just h' -> progShow (gd ^. gdHabData . _HabData . ixSV h' . habName . unpacked)
+                                    (c - fromMaybe 0 a) c
+                )
+                 <$> (fs ^. fsHabs . hsSlots) <*> habCaps <*> avails
+               )
+      where
+        progShow :: String -> Double -> Double -> Widget n
+        progShow s x y = progressBar (Just $ printf "%s (%d / %d)" s (round @_ @Int x) (round @_ @Int y))
+                                     (realToFrac (x / y))
+        habCaps = habCapacities (gd ^. gdHabData) bs (fs ^. fsHabs)
+        avails  = fs ^. fsHabs . availableSpaces (gd ^. gdHabData) bs
 
 app
     :: (KnownNat eggs, SingI tiers, KnownNat epic, KnownNat habs, KnownNat vehicles, 1 TL.<= eggs, 1 TL.<= habs, 1 TL.<= vehicles)
@@ -34,23 +65,29 @@ app
     -> App (FarmStatus eggs '(tiers, epic) habs vehicles) Double ()
 app gd = App draw cursor handle return am
   where
-    draw fs = (:[]) . vBox $
-      [ str "Egg Simulator"
-      , str $ gd ^. gdEggData . _EggData . ixSV (fs ^. fsEgg) . eggName
-      , str . show $ fs ^. fsBocks
-      , str . show $ totalChickens (gd ^. gdHabData) (fs ^. fsHabs)
-      , str . show $ totalHabCapacity (gd ^. gdHabData) (farmBonuses gd fs) (fs ^. fsHabs)
+    draw fs = [renderApp gd fs]
+        -- [ str "Egg Simulator"
+        -- , txt $ gd ^. gdEggData . _EggData . ixSV (fs ^. fsEgg) . eggName
+        -- , str . show $ fs ^. fsBocks
+        -- , str $ printf "%d / %d" (round @_ @Int cs) (round @_ @Int hcap)
+        -- , progressBar (Just "Habs") (realToFrac $ cs / hcap)
+        -- ]
+      -- where
+        -- cs = totalChickens (gd ^. gdHabData) (fs ^. fsHabs)
+        -- hcap = totalHabCapacity (gd ^. gdHabData) (farmBonuses gd fs) (fs ^. fsHabs)
 
-      ]
     cursor _ _ = Nothing
     handle fs0 = \case
       VtyEvent e  -> case e of
-        EvKey (KChar 'q') _   -> halt fs0
+        EvKey (KChar 'q') _ -> halt fs0
         EvKey (KChar ' ') _ -> nextRight fs0 $ runAction gd (AHatch 1) fs0
         _              -> continue fs0
       AppEvent dt -> continue $ stepFarmDT gd NotCalm dt fs0
       _           -> continue fs0
-    am _ = attrMap defAttr []
+    am _ = attrMap defAttr $
+        [(progressIncompleteAttr, white `on` black)
+        ,(progressCompleteAttr  , black `on` white)
+        ]
 
 main :: IO ()
 main = do
--- src/Egg.hs
+++ src/Egg.hs
@@ -8,93 +8,14 @@
 {-# LANGUAGE TypeOperators        #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-module Egg where
--- module Egg (
---   ) where
+module Egg (
+  module E
+  ) where
 
--- import           Data.Type.Fin
--- import           Data.Type.Nat            as TCN
--- import qualified Type.Family.Nat
-import           Control.Monad.Trans.Maybe
-import           Data.Dependent.Map
-import           Data.Finite
-import           Data.Kind
-import           Data.Singletons
-import           Data.Singletons.Prelude.Num
-import           Data.Singletons.TH
-import           Data.Singletons.TypeLits
-import           Data.Type.Combinator
-import           GHC.TypeLits
-import           Numeric.Natural
-import qualified Data.Vector.Sized           as V
-
-$(promote [d|
-  numUpgrades :: (Eq a, Num a) => a -> a
-  numUpgrades i = case i of
-    0  -> 4
-    1  -> 5
-    2  -> 5
-    3  -> 5
-    4  -> 5
-    5  -> 4
-    6  -> 4
-    7  -> 4
-    8  -> 4
-    9  -> 4
-    10 -> 3
-    _  -> 0
-  |])
-
-newtype (:.~:) :: (l -> Type) -> (k ~> l) -> k -> Type where
-    CompTyFun :: f (Apply g a) -> (f :.~: g) a
-
--- newtype Upgrade :: Nat -> Type where
---     Upgr :: Finite (NumUpgrades n) -> Upgrade n
-
--- newtype Upgrades :: Nat -> Type where
---     Upgrs :: V.Vector (NumUpgrades n) Natural -> Upgrades n
-
-type HabSlot = Finite 4
-type Hab = Finite 18
-
-type VehicleSlot = Finite 14
-type Vehicle = Finite 11
-
-data Move :: Type where
-    MResearch
-        :: (DSum Sing (Finite :.~: NumUpgradesSym0))
-        -> Move
-    MHabUpgrade
-        :: HabSlot -> Hab -> Move
-    MVehicleUpgrade
-        :: VehicleSlot -> Vehicle -> Move
-    MFleetUpgrade
-        :: Move
-    MWaitChickens
-        :: Natural -> Move
-    MWaitBocks
-        :: Double -> Move
-
--- data FarmShape :: Type where
---     FS :: { fsFleetSize :: Nat
---           }
---        -> FarmShape
-
-data Farm :: Type where
-    Farm :: { fBocks     :: Double
-            , fChickens  :: Natural
-            , fUpgrades  :: DMap Sing (Flip V.Vector Natural :.~: NumUpgradesSym0)
-            , fHabitats  :: V.Vector 4 Hab
-            , fVehicles  :: MaybeT (V.Vector 14) Hab
-            , fSoulEnggs :: Natural
-            }
-         -> Farm
-
-data Bonus :: Type where
-    Bonus :: { bEggValue   :: Double
-             , bFarmValue  :: Double
-             , bLayingRate :: Double
-             , bHabSize    :: Double
-             }
-          -> Bonus
-
+import           Egg.Action    as E
+import           Egg.Commodity as E
+import           Egg.Egg       as E
+import           Egg.Farm      as E
+import           Egg.GameData  as E
+import           Egg.Habitat   as E
+import           Egg.Vehicle   as E
