ddc981ba01166294f5e795f55d4d4fbab248f1a8
Justin Le <justin@jle.im>
2017-07-03 15:25:33 -0700

    showing bocks

--- src/Egg/Commodity.hs
+++ src/Egg/Commodity.hs
@@ -12,13 +12,35 @@ module Egg.Commodity (
 
 import           Control.Lens
 import           Data.Aeson
+import           Data.Maybe
+import           Numeric.Lens
+import           Text.Printf
+import qualified Data.Vector  as V
 
 newtype Bock = Bock { _bock :: Double }
   deriving (Eq, Ord, Num, Fractional, Floating, Real, RealFrac, RealFloat, ToJSON, FromJSON)
 
-instance Show Bock
-
 makeLenses ''Bock
+
+instance Show Bock where
+  show = showBock
+
+showBock :: Bock -> String
+showBock b
+    | digs <= 2 = printf "%.2f" (b ^. bock)
+    | digs <= 6 = printf "%.0f" (b ^. bock)
+    | otherwise = printf "%.3f%s" (b ^. bock . dividing (10 ^ (pow3 * 3))) powpre
+  where
+    digs = floor $ logBase 10 b
+    pow3 = digs `div` 3
+    powpre = fromMaybe (printf "e%d" (pow3*3)) $ powpres V.!? pow3
+
+powpres :: V.Vector String
+powpres = V.fromList [ "" , "" , "M", "B"
+                     , "T", "q", "Q", "s"
+                     , "S", "o", "N", "d"
+                     , "U", "D", "Td", "qd"
+                     , "Qd", "sd", "Sd"]
 
 -- type Bock      = Double
 type GoldenEgg = Integer
